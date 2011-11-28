3c9b0add8fb7c1c63bc695474110eef39c22360d
林 秀一 <shuichih@rins-MacBook-Air.local>
2011-11-28 09:46:43 +0900

    Initial Commit

---
+++ rt/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  rt
+//
+//  Created by 秀一 林 on 11/28/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
